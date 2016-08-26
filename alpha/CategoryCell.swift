1bf5a3125edea675fae6cc4d4b7c05894598a1f0
Ezra <ezrabathini@gmail.com>
2016-08-27 02:47:57 +1200

    category list is a collectionview,.. finally 💪😏

---
+++ alpha/CategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCell.swift
+//  alpha
+//
+//  Created by Ezra Bathini on 27/08/16.
+//  Copyright © 2016 Ezra Bathini. All rights reserved.
+//
+
+import UIKit
+import Material
+
+class CategoryCell: MaterialCollectionViewCell {
+    
+    @IBOutlet weak var categoryImage: AsyncImageView!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+}
